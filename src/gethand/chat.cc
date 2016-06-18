a57b1dffdc32836e0ab40a23dbe986b7b2d738a0
ycseo <ycseo@hancom.com>
2016-06-18 19:57:08 +0900

    leap motion

--- src/gethand/chat.cc
+++ src/gethand/chat.cc
@@ -27,7 +27,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <signal.h>
+#include <iostream>
+#include <cstring>
+#include "Leap.h"
 
+using namespace Leap;
 using namespace ajn;
 
 /* constants. */
@@ -40,41 +44,6 @@ static ajn::BusAttachment* s_bus = NULL;
 static qcc::String s_advertisedName = "org.alljoyn.bus.samples.chat.iot_hand";
 static qcc::String s_sessionHost;
 static SessionId s_sessionId = 0;
-static volatile sig_atomic_t s_interrupt = false;
-
-static void CDECL_CALL SigIntHandler(int sig)
-{
-    QCC_UNUSED(sig);
-    s_interrupt = true;
-}
-
-/*
- * get a line of input from the the file pointer (most likely stdin).
- * This will capture the the num-1 characters or till a newline character is
- * entered.
- *
- * @param[out] str a pointer to a character array that will hold the user input
- * @param[in]  num the size of the character array 'str'
- * @param[in]  fp  the file pointer the sting will be read from. (most likely stdin)
- *
- * @return returns the same string as 'str' if there has been a read error a null
- *                 pointer will be returned and 'str' will remain unchanged.
- */
-char*get_line(char*str, size_t num, FILE*fp)
-{
-    char*p = fgets(str, num, fp);
-
-    // fgets will capture the '\n' character if the string entered is shorter than
-    // num. Remove the '\n' from the end of the line and replace it with nul '\0'.
-    if (p != NULL) {
-        size_t last = strlen(str) - 1;
-        if (str[last] == '\n') {
-            str[last] = '\0';
-        }
-    }
-
-    return s_interrupt ? NULL : p;
-}
 
 /* Bus object */
 class ChatObject : public BusObject {
@@ -285,19 +254,162 @@ QStatus AdvertiseName(TransportMask mask)
     return status;
 }
 
-/** Take input from stdin and send it as a chat message, continue until an error or
- * SIGINT occurs, return the result status. */
-QStatus DoTheChat(void)
-{
-    const int bufSize = 1024;
-    char buf[bufSize];
-    QStatus status = ER_OK;
+class SampleListener : public Listener {
+public:
+	virtual void onInit(const Controller&);
+	virtual void onConnect(const Controller&);
+	virtual void onDisconnect(const Controller&);
+	virtual void onExit(const Controller&);
+	virtual void onFrame(const Controller&);
+	virtual void onFocusGained(const Controller&);
+	virtual void onFocusLost(const Controller&);
+	virtual void onDeviceChange(const Controller&);
+	virtual void onServiceConnect(const Controller&);
+	virtual void onServiceDisconnect(const Controller&);
+	virtual void onServiceChange(const Controller&);
+	virtual void onDeviceFailure(const Controller&);
+	virtual void onLogMessage(const Controller&, MessageSeverity severity, int64_t timestamp, const char* msg);
+};
 
-    while ((ER_OK == status) && (get_line(buf, bufSize, stdin))) {
-        status = s_chatObj->SendChatSignal(buf);
-    }
+const std::string fingerNames[] = { "Thumb", "Index", "Middle", "Ring", "Pinky" };
+const std::string boneNames[] = { "Metacarpal", "Proximal", "Middle", "Distal" };
 
-    return status;
+void SampleListener::onInit(const Controller& controller) {
+	std::cout << "Initialized" << std::endl;
+}
+
+void SampleListener::onConnect(const Controller& controller) {
+	std::cout << "Connected" << std::endl;
+}
+
+void SampleListener::onDisconnect(const Controller& controller) {
+	// Note: not dispatched when running in a debugger.
+	std::cout << "Disconnected" << std::endl;
+}
+
+void SampleListener::onExit(const Controller& controller) {
+	std::cout << "Exited" << std::endl;
+}
+
+void SampleListener::onFrame(const Controller& controller) {
+	// Get the most recent frame and report some basic information
+	const Frame frame = controller.frame();
+	std::cout << "Frame id: " << frame.id()
+		<< ", timestamp: " << frame.timestamp()
+		<< ", hands: " << frame.hands().count()
+		<< ", extended fingers: " << frame.fingers().extended().count() << std::endl;
+
+	HandList hands = frame.hands();
+	for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
+		// Get the first hand
+		const Hand hand = *hl;
+		std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+		std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+			<< ", palm position: " << hand.palmPosition() << std::endl;
+		// Get the hand's normal vector and direction
+		const Vector normal = hand.palmNormal();
+		const Vector direction = hand.direction();
+
+		// Calculate the hand's pitch, roll, and yaw angles
+		std::cout << std::string(2, ' ') << "pitch: " << direction.pitch() * RAD_TO_DEG << " degrees, "
+			<< "roll: " << normal.roll() * RAD_TO_DEG << " degrees, "
+			<< "yaw: " << direction.yaw() * RAD_TO_DEG << " degrees" << std::endl;
+
+		// Get the Arm bone
+		Arm arm = hand.arm();
+		std::cout << std::string(2, ' ') << "Arm direction: " << arm.direction()
+			<< " wrist position: " << arm.wristPosition()
+			<< " elbow position: " << arm.elbowPosition() << std::endl;
+
+		// Get fingers
+		const FingerList fingers = hand.fingers();
+		for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
+			const Finger finger = *fl;
+			std::cout << std::string(4, ' ') << fingerNames[finger.type()]
+				<< " finger, id: " << finger.id()
+				<< ", length: " << finger.length()
+				<< "mm, width: " << finger.width() << std::endl;
+
+			// Get finger bones
+			for (int b = 0; b < 4; ++b) {
+				Bone::Type boneType = static_cast<Bone::Type>(b);
+				Bone bone = finger.bone(boneType);
+				std::cout << std::string(6, ' ') << boneNames[boneType]
+					<< " bone, start: " << bone.prevJoint()
+					<< ", end: " << bone.nextJoint()
+					<< ", direction: " << bone.direction() << std::endl;
+			}
+		}
+	}
+
+
+	s_chatObj->SendChatSignal(std::to_string(GetTickCount()).data());
+
+	if (!frame.hands().isEmpty()) {
+		std::cout << std::endl;
+	}
+
+}
+
+void SampleListener::onFocusGained(const Controller& controller) {
+	std::cout << "Focus Gained" << std::endl;
+}
+
+void SampleListener::onFocusLost(const Controller& controller) {
+	std::cout << "Focus Lost" << std::endl;
+}
+
+void SampleListener::onDeviceChange(const Controller& controller) {
+	std::cout << "Device Changed" << std::endl;
+	const DeviceList devices = controller.devices();
+
+	for (int i = 0; i < devices.count(); ++i) {
+		std::cout << "id: " << devices[i].toString() << std::endl;
+		std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+		std::cout << "  isSmudged:" << (devices[i].isSmudged() ? "true" : "false") << std::endl;
+		std::cout << "  isLightingBad:" << (devices[i].isLightingBad() ? "true" : "false") << std::endl;
+	}
+}
+
+void SampleListener::onServiceConnect(const Controller& controller) {
+	std::cout << "Service Connected" << std::endl;
+}
+
+void SampleListener::onServiceDisconnect(const Controller& controller) {
+	std::cout << "Service Disconnected" << std::endl;
+}
+
+void SampleListener::onServiceChange(const Controller& controller) {
+	std::cout << "Service Changed" << std::endl;
+}
+
+void SampleListener::onDeviceFailure(const Controller& controller) {
+	std::cout << "Device Error" << std::endl;
+	const Leap::FailedDeviceList devices = controller.failedDevices();
+
+	for (FailedDeviceList::const_iterator dl = devices.begin(); dl != devices.end(); ++dl) {
+		const FailedDevice device = *dl;
+		std::cout << "  PNP ID:" << device.pnpId();
+		std::cout << "    Failure type:" << device.failure();
+	}
+}
+
+void SampleListener::onLogMessage(const Controller&, MessageSeverity s, int64_t t, const char* msg) {
+	switch (s) {
+	case Leap::MESSAGE_CRITICAL:
+		std::cout << "[Critical]";
+		break;
+	case Leap::MESSAGE_WARNING:
+		std::cout << "[Warning]";
+		break;
+	case Leap::MESSAGE_INFORMATION:
+		std::cout << "[Info]";
+		break;
+	case Leap::MESSAGE_UNKNOWN:
+		std::cout << "[Unknown]";
+	}
+	std::cout << "[" << t << "] ";
+	std::cout << msg << std::endl;
 }
 
 int CDECL_CALL main(int argc, char** argv)
@@ -311,9 +423,6 @@ int CDECL_CALL main(int argc, char** argv)
         return 1;
     }
 #endif
-
-    /* Install SIGINT handler. */
-    signal(SIGINT, SigIntHandler);
 
     QStatus status = ER_OK;
 
@@ -368,9 +477,36 @@ int CDECL_CALL main(int argc, char** argv)
             status = AdvertiseName(SERVICE_TRANSPORT_TYPE);
         }
 
-        if (ER_OK == status) {
-            status = DoTheChat();
-        }
+		// Create a sample listener and controller
+		SampleListener listener;
+		Controller controller;
+
+		// Have the sample listener receive events from the controller
+		controller.addListener(listener);
+
+		if (argc > 1 && strcmp(argv[1], "--bg") == 0)
+			controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
+
+		controller.setPolicy(Leap::Controller::POLICY_ALLOW_PAUSE_RESUME);
+
+		// Keep this process running until Enter is pressed
+		std::cout << "Press Enter to quit, or enter 'p' to pause or unpause the service..." << std::endl;
+
+		bool paused = false;
+		while (true) {
+			char c = std::cin.get();
+			if (c == 'p') {
+				paused = !paused;
+				controller.setPaused(paused);
+				std::cin.get(); //skip the newline
+			}
+			else
+				break;
+		}
+
+		// Remove the sample listener when done
+		controller.removeListener(listener);
+
     } else {
         status = ER_OUT_OF_MEMORY;
     }
